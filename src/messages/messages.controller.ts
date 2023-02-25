506631d9b27a2279a795feb8c3eae5adf66ab526
Abhishek Singh <singh4758@gmail.com>
2023-02-25 20:14:21 +0530

    feat: access information of request  with the help of decorator

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
-  listMessage() {}
+  listMessage(@Param('id') id: string) {
+    console.log(id);
+  }
   @Get('/:id')
   getMessage() {}
   @Post()
-  createMessage() {}
+  createMessage(@Body() body: any) {
+    console.log(body);
+  }
 }
