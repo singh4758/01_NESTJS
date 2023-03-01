ede124fb78ebd9a79579066d16861982cf2e2151
Abhishek Singh <singh4758@gmail.com>
2023-03-01 20:31:30 +0530

    feat: add consuming multiple model

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create();
+  const app = await NestFactory.create(ComputerModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
