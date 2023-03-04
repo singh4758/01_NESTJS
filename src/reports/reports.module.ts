e7e9637308256a3570f0927182a030c93bb8b6c5
Abhishek Singh <singh4758@gmail.com>
2023-03-04 23:40:09 +0530

    fix: fix lint

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 
 @Module({
-  providers: [ReportsService]
+  providers: [ReportsService],
 })
 export class ReportsModule {}
