5b7789b8f64d9c41c8af14ba7844ffac7b2fb24b
Abhishek Singh <singh4758@gmail.com>
2023-03-04 23:34:13 +0530

    feat: generate users and reports module and do setup for sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      database: 'db.sqlite',
+      entities: [],
+      type: 'sqlite',
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
