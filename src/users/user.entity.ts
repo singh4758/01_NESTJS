ca9d695a230c3e32ac341c8449775526864a50a3
Abhishek Singh <singh4758@gmail.com>
2023-03-05 11:39:14 +0530

    feat understand relationship between create, save and entity hooks

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -8,4 +15,16 @@ export class User {
   email: string;
   @Column()
   password: string;
+  @AfterInsert()
+  logInsert() {
+    console.log('insert with id', this.id);
+  }
+  @AfterUpdate()
+  logUpdate() {
+    console.log('update with id', this.id);
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log('remove with id', this.id);
+  }
 }
