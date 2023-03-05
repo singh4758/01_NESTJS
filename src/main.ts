5bca212ae3dbd31b5ff6cd4873ec754aaa4a2a65
Abhishek Singh <singh4758@gmail.com>
2023-03-05 10:56:39 +0530

    fix: with the help of whitelist true we can remove extra properties which is not define in its redpective dtos

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
+      whitelist: true, // here whitelist true means mark that user will not send extra property
+      // if it is then remove from request body
     }),
   );
   await app.listen(3000);
