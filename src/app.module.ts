16e58efbee6e7802cfe4ab1d2c0e23901a65aa0d
Abhishek Singh <singh4758@gmail.com>
2023-03-05 07:32:43 +0530

    feat: create a report entity ans add in Report and App module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Report],
       type: 'sqlite',
       synchronize: true,
     }),
